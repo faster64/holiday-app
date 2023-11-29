8c927652ff11354c15e8ae5a2ef533e03fa2b520
cuong.nv3@kiotviet.com <cuong.nv3@kiotviet.com>
2023-11-29 15:26:31 +0700

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-    if (environment.production) {
+    if (environment.production || true) {
       this.logGreeting();
     }
     this.handleRouteChange();
@@ -48,14 +48,16 @@ export class AppComponent implements OnInit, OnDestroy {
 
       if (event instanceof NavigationEnd) {
         this.inProgress = false;
-        console.log(this.activatedRoute);
         this.path = (this.activatedRoute.snapshot as any)._routerState.url;
       }
     });
   }
 
   logGreeting() {
-    console.log('%cStop!', 'font-size: 64px; font-weight: bold; color: red; padding: 64px');
+    console.log('%cContact for work!', 'font-size: 32px; font-weight: bold; color: red; padding: 0 32px');
+    console.log('%cCương Nguyễn', 'font-size: 32px; font-weight: bold; color: red; padding: 0 32px');
+    console.log('%cMail: cuongnguyen.ftdev@gmail.com', 'font-size: 32px; font-weight: bold; color: red; padding: 0 32px');
+    console.log('%cPhone: 0847884444', 'font-size: 32px; font-weight: bold; color: red; padding: 0 32px');
   }
 
   ngOnDestroy(): void {
